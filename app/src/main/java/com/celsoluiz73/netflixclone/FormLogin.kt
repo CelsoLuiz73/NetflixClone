a7aedf66854004f4a45555387483b3f801fb76d4
CelsoLuiz73 <oclinfo21@gmail.com>
2022-09-22 00:47:52 -0300

    Autenticacao Usuario, Clique ir para tela de filmes 22set2022

--- app/src/main/java/com/celsoluiz73/netflixclone/FormLogin.kt
+++ app/src/main/java/com/celsoluiz73/netflixclone/FormLogin.kt
@@ -3,7 +3,9 @@ package com.celsoluiz73.netflixclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.celsoluiz73.netflixclone.databinding.ActivityFormLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class FormLogin : AppCompatActivity() {
 
@@ -21,5 +23,36 @@ class FormLogin : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.btnCadastrar.setOnClickListener {
+            val email = binding.etxEmailCadastro.text.toString()
+            val senha = binding.etxSenhaCadastro.text.toString()
+            val mensagemErro = binding.tvInformeEmailSenha
+            if (email.isEmpty() || senha.isEmpty()) {
+                mensagemErro.setText("Preencha todos os campos!")
+            } else {
+                autenticarUsuario()
+            }
+        }
+    }
+
+    private fun autenticarUsuario() {
+        val email = binding.etxEmailCadastro.text.toString()
+        val senha = binding.etxSenhaCadastro.text.toString()
+        val mensagemErro = binding.tvInformeEmailSenha
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show()
+                irParaTelaFilmes()
+            }
+        }.addOnFailureListener {
+            mensagemErro.setText("Erro ao logar usuário")
+        }
+    }
+
+    private fun irParaTelaFilmes() {
+        val intent = Intent(this, ListaFilmes::class.java)
+        startActivity(intent)
+        finish()
     }
 }
